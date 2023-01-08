8579371e6be53620896f6943c06f80cd88fbf8e3
olga <olga.szat@gmail.com>
2023-01-08 17:17:28 +0100

    added posibility to define width at component level

--- frontend/warehouse-app/src/components/TextInputField.js
+++ frontend/warehouse-app/src/components/TextInputField.js
@@ -1,12 +1,11 @@
 export default function TextInputField({ id, label, setState }) {
-  // to define width wrap component in div and use "flex w-x" to style
   return (
       <input
         type="text"
         id={id}
         onChange={setState}
         placeholder={label}
-        className="appearance-none border border-gray-700 rounded-full w-full py-2 px-4"
+        className="appearance-none border border-gray-700 rounded-full w-100 py-2 px-4"
         required
       />
   );
