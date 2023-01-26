74a1fa05799442e53653b42be89716a27acba196
Agata <agata.piasecka@outlook.com>
2023-01-26 02:04:43 +0100

    Added forms

--- frontend/warehouse-app/src/pages/Login.js
+++ frontend/warehouse-app/src/pages/Login.js
@@ -27,29 +27,31 @@ export default function Login() {
   return (
     <div className="w-full h-screen bg-primaryBlue flex justify-center align-middle items-center">
       <p className="absolute top-4 left-4 font-thin text-sm text-white">Warehouse  <br/> Management <br/> System </p>
-      <div className="flex flex-col gap-4 items-center">
-        <h1 className="text-2xl font-thin text-white">Please log in</h1>
-        <TextInputField
-          label="Username"
-          width="w-80"
-          value={data.username}
-          onValueChange={onUsernameChange} />
-        <TextInputField
-          label="Password" 
-          width="w-80" 
-          value={data.password}
-          onValueChange={onPassowrdChange} />
-        <div className="flex flex-row gap-4 items-center">
-          <Button
-            label="Log in"
-            onClick={onLogin} />
-          <a href="/sign-up">
-          <Button
-            label="Create account"
-            color="transparent" />
-          </a>
+      <form onSubmit={onLogin}>
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="text-2xl font-thin text-white">Please log in</h1>
+          <TextInputField
+            label="Username"
+            width="w-80"
+            value={data.username}
+            onValueChange={onUsernameChange} />
+          <TextInputField
+            label="Password" 
+            width="w-80" 
+            value={data.password}
+            onValueChange={onPassowrdChange} />
+          <div className="flex flex-row gap-4 items-center">
+            <Button
+              label="Log in"
+              type="submit" />
+            <a href="/sign-up">
+            <Button
+              label="Create account"
+              color="transparent" />
+            </a>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
--- frontend/warehouse-app/src/pages/SignUp.js
+++ frontend/warehouse-app/src/pages/SignUp.js
@@ -43,30 +43,32 @@ export default function SignUp() {
     <div className="w-full h-screen bg-primaryBlue flex justify-center align-middle items-center">
       <p className="absolute top-4 left-4 font-thin text-sm text-white">Warehouse  <br/> Management <br/> System </p>
       <a className="absolute bottom-4 left-4 font-thin text-sm text-white" href="/login"> Return </a>
-      <div className="flex flex-col gap-4 items-center">
-        <h1 className="text-2xl font-thin text-white">Sign up</h1>
-        <TextInputField
-          label="Username"
-          width="w-80"
-          value={data.username}
-          onValueChange={onUsernameChange} />
-        <TextInputField
-          label="Password" 
-          width="w-80"
-          value={data.password}
-          onValueChange={onPassowrdChange} />
-        <TextInputField
-          label="Confirm password" 
-          width="w-80"
-          value={data.passwordConfirm}
-          onValueChange={onPasswordConfirmChange} />
-        <LoginDropdown onRoleChange={onRoleChange}/>
-        <div>
-          <Button
-            label="Create account"
-            onClick={onCreateAccount} />
+      <form onSubmit={onCreateAccount}>
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="text-2xl font-thin text-white">Sign up</h1>
+          <TextInputField
+            label="Username"
+            width="w-80"
+            value={data.username}
+            onValueChange={onUsernameChange} />
+          <TextInputField
+            label="Password" 
+            width="w-80"
+            value={data.password}
+            onValueChange={onPassowrdChange} />
+          <TextInputField
+            label="Confirm password" 
+            width="w-80"
+            value={data.passwordConfirm}
+            onValueChange={onPasswordConfirmChange} />
+          <LoginDropdown onRoleChange={onRoleChange}/>
+          <div>
+            <Button
+              label="Create account"
+              type="submit" />
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
