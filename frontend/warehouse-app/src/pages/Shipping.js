4000f0e25ff1944efb2e4679864d77a70fd91d1f
agatapias <agata.piasecka@outlook.com>
2023-01-28 23:13:55 +0100

    Added all shipping requests.

--- frontend/warehouse-app/src/pages/Shipping.js
+++ frontend/warehouse-app/src/pages/Shipping.js
@@ -1,109 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PageContainer from '../components/containers/PageContainer';
 import ShippingDeliveryTextBox from '../components/ShippingDeliveryTextBox';
 import AddEditShippingDialog from '../components/shippings/AddEditShippingDialog';
 import RemoveShippingDialog from '../components/shippings/RemoveShippingDialog';
   
-export default function Shipping({user}) {
+export default function Shipping({user, warehouseId}) {
 
-  const [shippings, setShippings] = useState([
-    {
-      id: 1,
-      assignedTo: 2,
-      plannedDate: "02/12/2023",
-      status: "ongoing",
-      address: {
-        street: "Ścinawska",
-        number: 2,
-        postalCode: "42-900",
-        city: "Wrocław"
-      },
-      products: [
-        {
-          id: 1,
-          quantity: 2,
-          name: "mydło"
-        },
-        {
-          id: 2,
-          quantity: 10,
-          name: "food"
-        },
-      ]
-    },
-    {
-      id: 2,
-      assignedTo: 5,
-      plannedDate: "02/03/2023",
-      status: "ongoing",
-      address: {
-        street: "Ścinawska",
-        number: 2,
-        postalCode: "42-900",
-        city: "Wrocław"
-      },
-      products: [
-        {
-          id: 1,
-          quantity: 2,
-          name: "mydło"
-        },
-        {
-          id: 2,
-          quantity: 10,
-          name: "food"
-        },
-      ]
-    },
-    {
-      id: 3,
-      assignedTo: 1,
-      plannedDate: "01/05/2023",
-      status: "ongoing",
-      address: {
-        street: "Ścinawska",
-        number: 2,
-        postalCode: "42-900",
-        city: "Wrocław"
-      },
-      products: [
-        {
-          id: 1,
-          quantity: 2,
-          name: "mydło"
-        },
-        {
-          id: 2,
-          quantity: 10,
-          name: "food"
-        },
-      ]
-    },
-    {
-      id: 4,
-      assignedTo: 1,
-      plannedDate: "01/05/2023",
-      status: "ongoing",
-      address: {
-        street: "Ścinawska",
-        number: 2,
-        postalCode: "42-900",
-        city: "Wrocław"
-      },
-      products: [
-        {
-          id: 1,
-          quantity: 2,
-          name: "mydło"
-        },
-        {
-          id: 2,
-          quantity: 10,
-          name: "food"
-        },
-      ]
+  const [shippings, setShippings] = useState([]);
+
+  const getApiData = async () => {
+    let res = await fetch(`http://localhost:8080/orders`, { 
+      method: 'GET',
+      credentials: 'include',
+      mode: 'cors',
+      referrerPolicy: 'no-referrer',
+      origin: "http://localhost:3000/",
+    })
+
+    if (res.status === 200) {
+      const json = await res.json();
+      console.log("Successfully loaded data.");
+      setShippings(json);
+    } else {
+      console.log("Could not load data.");
     }
-  ]);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
 
   if (user==="ROLE_EMPLOYEE") {
     return (
@@ -130,25 +55,29 @@ export default function Shipping({user}) {
       <PageContainer title="Shippings from" location="Świdnicka 24">
         <div className='flex flex-col gap-4'>
           <AddEditShippingDialog 
-            isDeliveryOrShipping="shipping" 
-            isAddOrEdit="add"
+            warehouseId={warehouseId}
+            isEdit={false}
             buttonLabel="Add new" 
-            title="Add shipping"/>
+            title="Add shipping"
+            updateList={getApiData} />
           {shippings.map( shipping =>
             <div>
               <div className='flex flex-row gap-4 items-center pb-2'>
                 <h1 className="text-2xl font-thin">{`Shipping no ${shipping.id}`}</h1>
                 <AddEditShippingDialog 
-                  isDeliveryOrShipping="shipping" 
-                  isAddOrEdit="edit"
+                  warehouseId={warehouseId}
+                  isEdit={true}
+                  shippingId={shipping.id}
                   buttonLabel="Edit" 
                   buttonColor="white" 
-                  title="Edit shipping"/>
+                  title="Edit shipping"
+                  updateList={getApiData} />
                 <RemoveShippingDialog 
                   shippingId={shipping.id}
                   buttonLabel="Remove" 
                   buttonColor="gray" 
-                  title="Remove shipping"/>
+                  title="Remove shipping"
+                  updateList={getApiData} />
               </div>
               <ShippingDeliveryTextBox
               assignedTo={shipping.employee.id}
